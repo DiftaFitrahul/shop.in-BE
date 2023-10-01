76265a8c8cf8c62c083cd1f775ab2ad1d6a5ce1a
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-10-01 15:20:20 +0700

    feat: succes connect to postgresql database

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('./util/database')
+
+const app = express();
+
+app.use(bodyParser.json());
+
+sequelize.sync().then(async()=>{
+    console.log('succes connect to postgresql database')
+    app.listen(5000, ()=>{
+        console.log('server is running on port 5000')
+    });
+})
+
+
+
+module.exports = app
